67846752211cf4fa6ef7f09e1b781b567411a54e
alfredsn <alfred.nainggolan00@gmail.com>
2024-02-13 20:22:38 +0700

    change the signature of the findMissingNumber function to return a list of integers instead of a single integer

--- src/main/kotlin/cses_problem_set/MissingNumber.kt
+++ src/main/kotlin/cses_problem_set/MissingNumber.kt
@@ -27,21 +27,32 @@ Output:
 4
 */
 
-fun findMissingNumber(n: Int, arr: List<Int>): Int {
-    var res = 0
+fun findMissingNumber(n: Int, arr: List<Int>): List<Int> {
+    val missingNumbers = mutableListOf<Int>()
 
     if (n >= 0) {
         val totalSum = (n * (n + 1)) / 2
         val givenSum = arr.sum()
-        res = totalSum - givenSum
-    } else {
-        println("The value of n must be positive!")
-    }
+        val totalDifference = totalSum - givenSum
 
-    return res
+        if (totalDifference != 0) {
+            var current = 1
+            var remainingDifference = totalDifference
+
+            while (remainingDifference > 0) {
+                if (!arr.contains(current)) {
+                    missingNumbers.add(current)
+                    remainingDifference -= current
+                }
+                current++
+            }
+        }
+    }
+    return missingNumbers
 }
 
 fun main() {
-    val missingNumber = findMissingNumber(-6, listOf(2,3,1,5,4))
-    println(missingNumber)
+    val result = findMissingNumber(5, listOf(1, 4))
+    println(result)
 }
+
