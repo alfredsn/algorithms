0a3e0f89b7470038856de036c2b3e2eca5f64858
alfredsn <alfred.nainggolan00@gmail.com>
2024-02-27 11:03:56 +0700

    solve quick sort

--- src/main/kotlin/arrays/QuickSort.kt
+++ src/main/kotlin/arrays/QuickSort.kt
@@ -1,5 +1,15 @@
 package arrays
 
 fun quickSort(qArr: IntArray) {
+    if (qArr.size < 2) return
+    val pivot = qArr[0]
+    val left = qArr.filter { it < pivot }
+    val right = qArr.filter { it > pivot }
 
+    quickSort(left.toIntArray())
+    quickSort(right.toIntArray())
+
+    System.arraycopy(left, 0, qArr, 0, left.size)
+    qArr[left.size] = pivot
+    System.arraycopy(right, 0, qArr, left.size + 1, right.size)
 }
